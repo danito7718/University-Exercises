2976395307173dba278089f8610ed17fe8496d62
Daniel Trifonov <79804297+danito7718@users.noreply.github.com>
2025-01-05 00:04:36 +0200

    Exercises in C#

---
+++ MoreExercises/bonusSalary/BonusSalary.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Въведете името на работника:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Въведете заплатата на работника:");
+double salary = 0;
+salary = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Въведете месечния оборот:");
+double salesValue = 0;
+salesValue = double.Parse(Console.ReadLine());
+
+double bonus = (salesValue * 0.15);
+Console.WriteLine($"Общо: {(salary + bonus):f2}");
+
