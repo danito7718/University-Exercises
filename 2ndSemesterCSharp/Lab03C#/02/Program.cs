653e2898c609983dec421579475a27edddb09e3b
danito7718 <danito7718@gmail.com>
2025-03-09 01:10:47 +0200

    Още задачи от Лабораторните упражнения

---
+++ 2ndSemesterCSharp/Lab03C#/02/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Въведете големината на масива:");
+int n = int.Parse(Console.ReadLine());
+int index = 0;
+int[] array = new int[n];
+int posCounter = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Въведете елемент за масива с индекс {i}");
+    array[i] = int.Parse(Console.ReadLine());
+    if (array[i] >= 0)
+    {
+        posCounter++;
+    }
+}
+int[] posArray = new int[posCounter];
+Console.WriteLine();
+for (int i = 0;i < n; i++)
+{
+    if (array[i] >= 0)
+    {
+        posArray[index] = array[i];
+        index++;
+    }
+}
+Console.WriteLine("Масивът с позитивни елементи:");
+foreach (var item in posArray)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+Console.WriteLine("Оригиналният масив:");
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
---
+++ 2ndSemesterCSharp/Lab03C#/04/Program.cs
@@ -0,0 +1,54 @@
+﻿Dictionary<string, int> weatherData =  new Dictionary<string, int>
+{
+    {"Ново село", 8 },
+    { "Видин", 8},
+    { "Лом", 8},
+    { "Оряхово", 8},
+    { "Монтана", 15},
+    { "Враца", 14},
+    { "Кнежа", 8},
+    { "Ловеч", 10},
+    { "Плевен", 13},
+};   // Създаваме речник и вкарваме стойностите които знаем предварително!
+
+// Създаваме 2 променливи с най-малък и с най-голям размер, за да сравним по-късно и да намерим желаните стойности.
+int lowestTemperature = int.MaxValue; 
+int highestTemperature = int.MinValue;
+List<string> coldestStations = new List<string>();
+List<string> hottestStations = new List<string>();
+
+foreach (var item in weatherData) // Четем данните от речника и ги записваме в листа.
+{
+    if (item.Value < lowestTemperature) //четем entry.Value от 1вия елемент на речника
+    {
+        lowestTemperature = item.Value; // Добавяме Value(стойността на текущия елемент в речника) към листа
+        coldestStations.Clear(); // Премахваме информацията от листа ако имаме промяна (нова по-висока температура)!
+        coldestStations.Add(item.Key); //Добавяме key(string) към листа
+    }
+    else if (item.Value == lowestTemperature)
+        coldestStations.Add(item.Key); // Ако имаме еднаква температура добавяме я към съществуващия лист
+    
+
+    if (item.Value > highestTemperature) //четем item.Value от 1вия елемент на речника
+    {
+        highestTemperature = item.Value;
+        hottestStations.Clear();
+        hottestStations.Add(item.Key);
+    }
+    else if (item.Value == highestTemperature) // Ако имаме еднаква температура добавяме я към съществуващия лист с най-високи температури
+        hottestStations.Add(item.Key);
+}
+
+int temperatureDifference = highestTemperature - lowestTemperature; // намираме разликата в температурата
+
+if (coldestStations.Count > 1) // ако имаме повече от 1 най-студена станция, пишем (станциите)
+    Console.WriteLine($"Най-ниската измерена температура е: {lowestTemperature}°C, в станциите: {string.Join(", ", coldestStations)}");
+else
+    Console.WriteLine($"Най-ниската измерена температура е: {lowestTemperature}°C, в станцията: {coldestStations[0]}");
+
+if (hottestStations.Count > 1) //...
+    Console.WriteLine($"Най-високата измерена температура е: {highestTemperature}°C, в станциите: {string.Join(", ", hottestStations)}");
+else 
+    Console.WriteLine($"Най-високата измерена температура е: {highestTemperature}°C, в станцията: {hottestStations[0]}");
+
+Console.WriteLine($"\nТемпературната разлика е: {temperatureDifference}°C");
